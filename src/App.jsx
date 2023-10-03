b9e146e09e4c48f7138bc17896696896a152a6cf
Victor Álvarez Brosso <vbrosso@gmail.com>
2023-10-03 12:40:01 -0300

    estrutura arquivos

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState(null); // Inicialize user como null
+  const [number, setNumber] = useState(1);
+  const [inputValue, setInputValue] = useState(''); // Inicialize inputValue como uma string vazia
+  const [userNotFound, setUserNotFound] = useState(false); // Estado para rastrear se o usuário não foi encontrado
+
+  const changeNumber = () => {
+    setNumber((prevNumber) => prevNumber + 1);
+  }
+
+  const handleSearch = () => {
+    if (inputValue) {
+      fetch(`https://api.github.com/users/${inputValue}`)
+        .then((res) => {
+          if (res.status === 404) {
+            setUserNotFound(true); // Define o estado para indicar que o usuário não foi encontrado
+            setUser(null);
+            return null;
+          }
+          return res.json();
+        })
+        .then((json) => {
+          if (json) {
+            setUser(json);
+            setInputValue(json.login);
+            setUserNotFound(false); // Define o estado de volta para falso se o usuário for encontrado
+          }
+        })
+        .catch((error) => console.error("Erro ao buscar dados do usuário:", error));
+    }
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    setUserNotFound(false); // Limpa o estado de usuário não encontrado ao digitar no input
+  }
+
+  return (
+    <div className='App'>
+      <div>
+        <h2>Digite o seu nome de usuário do github:</h2>
+        <input
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSearch}>Buscar</button> {/* Botão de busca */}
+        {user && (
+            <div>
+            <p>Seu nome de usuario é {user.login}</p>
+            <p><strong>Nome:</strong> {user.name} </p>
+           
+            <p><strong>Location:</strong> {user.location}</p>
+            <img src={user.avatar_url} alt={user.name} />
+            <legend>Link da Api: <a href={user.url} target='_blank' title={user.name}>{user.name} </a></legend>
+          </div>
+        )}
+        
+      </div>
+
+      {/* Meu número é: {number}
+      <button onClick={changeNumber}>Mudar número!</button> */}
+    </div>
+  );
+}
+
+export default App;
